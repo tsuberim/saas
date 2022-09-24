c1ab996d6be6e09fe1c823dd16375d58f713dbe3
Matan Tsuberi <tsubeirm@gmail.com>
2022-09-24 02:36:57 -0700

    some saas begins

--- runtime/src/index.ts
+++ runtime/src/index.ts
@@ -5,12 +5,18 @@ import Redis from 'ioredis';
 import express from 'express';
 
 import { Isolate } from 'isolated-vm';
+import { MongoClient } from 'mongodb';
 
 
 const port = Number(process.env.PORT || '8080');
 const redisUrl = new URL(process.env.REDIS!)
+const mongoUrl = process.env.MONGO!;
 
 async function main() {
+    const mongo = await MongoClient.connect(mongoUrl);
+    const sys = mongo.db('sys');
+    const projects = sys.collection('projects');
+
     const redis = new Redis({
         keyPrefix: 'runtime:',
         username: redisUrl.username,
@@ -19,22 +25,45 @@ async function main() {
         port: Number(redisUrl.port)
     });
 
-    const code = 'Math.random()'
-
-    const vm = new Isolate({memoryLimit: 8});
-    const context = await vm.createContext({});
-    const script = await vm.compileScript(code, {});
-    await script.run(context, {timeout: 100});
-
     console.log('hello')
 
     const app = express();
     app.use(express.json());
 
-    app.get('/', async (req, res) => {
-        await redis.incr('key');
-        const value = await redis.get('key');
-        res.send('hello world: ' + value)
+    const api = express.Router();
+    api.post('/:uid/:project/:env', async (req, res) => {
+        const { uid, project, env } = req.params;
+        const { code } = req.body;
+        const id = `${uid}-${project}-${env}`;
+
+        await projects.updateOne({ _id: id }, { $set: { _id: id, uid, project, env, code } }, { upsert: true })
+
+        res.send('OK');
+    });
+    app.use('/api', api);
+
+    app.use('/user/:uid/:project/:env', async (req, res) => {
+        const { uid, project, env } = req.params;
+
+        try {
+            const id = `${uid}-${project}-${env}`;
+            const doc = await projects.findOne({ _id: id });
+
+            if (!doc) {
+                throw new Error(`Project ${id} not found`);
+            }
+
+            const code: string = doc.code;
+
+            const vm = new Isolate({ memoryLimit: 8 });
+            const context = await vm.createContext({});
+            const script = await vm.compileScript(code, {});
+            const result = await script.run(context, { timeout: 100 });
+            res.send(result);
+        } catch (e) {
+            res.status(400).send(e.message);
+        }
+
     })
 
     app.listen(port);
